b408049cd838e10f1ab3ae70c70448b26a460e66
J M Howitt <jmhowitt@mac.com>
2016-11-05 12:53:14 +0000

    crud staff

--- methods.go
+++ methods.go
@@ -77,7 +77,8 @@ func getStaffedit(c *gin.Context) {
 		createdOn    int64
 		updatedOn    int64
 	)
-_iId, created_on, updated_on FROM staff WHERE id = $1", qid).Scan(&id, &email, &username, &password, &phone, &address, &postcode, &firstname, &lastname, &companies_id, &stores_id, &dept_id, &siteowner, &active, &breaksId, &createdOn, &updatedOn)
+
+	err = db.QueryRow("SELECT id, email, username, password, phone, address, postcode, firstname, lastname, companies_id, stores_id, dept_id, siteowner, active, breaks_id, created_on, updated_on FROM staff WHERE id = $1", qid).Scan(&id, &email, &username, &password, &phone, &address, &postcode, &firstname, &lastname, &companies_id, &stores_id, &dept_id, &siteowner, &active, &breaksId, &createdOn, &updatedOn)
 
 	if err != nil {
 		c.String(http.StatusInternalServerError,
