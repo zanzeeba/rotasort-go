48c5857f8048f042e616db1000c42331ab4270db
J M Howitt <jmhowitt@mac.com>
2016-11-05 13:01:06 +0000

    crud staff

--- methods.go
+++ methods.go
@@ -56,7 +56,7 @@ func getStaff(c *gin.Context) {
 func getStaffedit(c *gin.Context) {
 
 	// the parameter - id of the question
-	qid := c.Param("sid")
+	sid := c.Param("sid")
 
 	var (
 		id           int64
