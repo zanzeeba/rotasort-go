458d8f36b8eea160d00d70015e881f260f5125c7
J M Howitt <jmhowitt@mac.com>
2016-11-09 16:06:34 +0000

    add template

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func main() {
 	//tasks routing block
 	//    -    -    -    -    -    -    -    -    -
 	// staff routing block
-	router.GET("/staff", tasks.getStaff)
+	router.GET("/staff", getStaff)
 
 	router.Run(":" + port)
 }
